cfcfff8bcf366cd959ff6750a566b02ea0829bd0
SaicharanSarikonda <52876325+saicharansarikonda@users.noreply.github.com>
2021-05-21 08:49:14 +0530

    Create App.js

---
+++ react_blog/src/App.js
@@ -0,0 +1,36 @@
+import Navbar from './Navbar.js';
+import Home from './Home.js';
+import {BrowserRouter as Router,Route,Switch} from 'react-router-dom';
+import Create from './Create.js';
+import BlogDetails from './BlogDetails.js';
+import NotFound from './NotFound.js';
+
+function App() {
+  const title = 'Welcome to the blog';
+
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className='content'>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound/>
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
